9b74e3f5f45bf14a30090c1f63013865b615955f
= <=>
2024-06-26 23:12:47 -0400

    Uploading chnges

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+
+  const [data , setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://swapi.dev/api/people/');
+        console.log('Fetched data:', response.data);  // Log data to the console
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);  // Log error to the console
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  } 
+  , []);
+ 
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
